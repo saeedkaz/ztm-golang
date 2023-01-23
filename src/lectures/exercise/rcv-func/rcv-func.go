5e10dee3a0ae443640f717c45526d9ef4dd0137f
saeedkaz <saeed.kazemy@gmail.com>
2023-01-23 19:43:11 +0330

    rvc functon done

--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -25,10 +25,11 @@ type Player struct {
 func (player *Player) maxStatSet(i int, j int) {
 	player.MaxEnergy = i
 	player.MaxHealth = j
+	player.printPlayer()
 
 }
 func (player *Player) statCH(i int, j int) {
-	fmt.Println(player)
+	player.printPlayer()
 	player.Health += i
 	player.Energy += j
 	if player.Health > player.MaxHealth {
@@ -45,12 +46,20 @@ func (player *Player) statCH(i int, j int) {
 		player.Energy = 0
 	}
 
-	fmt.Println(player)
+	player.printPlayer()
+}
+func (player *Player) printPlayer() {
+	fmt.Println()
+	fmt.Println("MaxEnergey: ", player.MaxEnergy)
+	fmt.Println("MaxHealth: ", player.MaxHealth)
+	fmt.Println("CurEnergey: ", player.Energy)
+	fmt.Println("CurHealth: ", player.Health)
+
 }
 
 func main() {
 	player1 := Player{}
 	player1.maxStatSet(5, 6)
 	player1.statCH(3, -1)
-	fmt.Println(player1)
+	player1.printPlayer()
 }
